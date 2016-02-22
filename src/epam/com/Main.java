95ba71e9f583bb2f33ad787718a09246748fce35
HrinchenkoDmytro <d.greenchenko@gmail.com>
2016-02-22 11:59:22 +0200

    Initial commit

---
+++ src/epam/com/Main.java
@@ -0,0 +1,30 @@
+package epam.com;
+
+/**
+ * Created by Любовь on 22.02.2016.
+ */
+public class Main {
+
+    private String ManuFacture;
+    private int serialNumber;
+    private float price;
+    private int quantityCPU;
+    private int frequencyCPU;
+
+    public String setManuFacture(){
+        return ManuFacture;
+    }
+    public int setSerialNumber(){
+        return serialNumber;
+    }
+
+    public float setPrice(){
+        return price;
+    }
+    public int getQuantityCPU(){
+        return quantityCPU;
+    }
+    public int getFrequencyCPU(){
+        return frequencyCPU;
+    }
+}
